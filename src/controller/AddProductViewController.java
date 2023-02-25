f1d950ee5c0de494661a6ee144e73c36985446e6
leonredman <leonproart@yahoo.com>
2023-02-25 09:16:40 -0700

    add product method

--- src/controller/AddProductViewController.java
+++ src/controller/AddProductViewController.java
@@ -6,13 +6,44 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Inventory;
+import model.Product;
 import wgu.inventoryApp.MainApplication;
 
 import java.io.IOException;
 
 public class AddProductViewController {
+    @FXML
+    private TextField addProductSearch;
+    @FXML
+    private TableColumn associatedProductPartId;
+    @FXML
+    private TableColumn associatedProductPartName;
+    @FXML
+    private TableColumn associatedProductPartInventory;
+    @FXML
+    private TableColumn associatedProductPartPrice;
+    @FXML
+    private TableView associatedProductTable;
+    @FXML
+    private TableView addProductTable;
+    @FXML
+    private TableColumn addProductIdTxt;
+    @FXML
+    private TableColumn addProductNameText;
+    @FXML
+    private TableColumn addProductStockTxt;
+    @FXML
+    private TableColumn addProductPriceTxt;
+    @FXML
+    private Button thirdButton;
+    @FXML
+    private TableView addProductNameTxt;
     @FXML
     private TextField addProductInvField;
     @FXML
@@ -31,7 +62,7 @@ public class AddProductViewController {
     public void addProdBackToMain(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
         //Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-    Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
 
         Scene scene = new Scene(root, 1000, 450);
         stage.setTitle("Back To Main Screen");
@@ -40,5 +71,34 @@ public class AddProductViewController {
     }
 
     public void onProductSearch(ActionEvent actionEvent) {
+    }
+
+    public void onAddProductBtn(ActionEvent actionEvent) {
+    }
+
+    public void onSaveProductBtn(ActionEvent actionEvent) throws IOException{
+         int id = Integer.parseInt(addProductIdField.getText());
+         String name = addProductNameField.getText();
+         int stock = Integer.parseInt(addProductInvField.getText());
+         Double price = Double.parseDouble(addProductPriceField.getText());
+         int max = Integer.parseInt(addProductMaxField.getText());
+         int min = Integer.parseInt(addProductMinField.getText());
+
+
+        Product addProduct = new Product(id, name, stock, price, max, min);
+        Inventory.addProduct(addProduct);
+
+        Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
+        //Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root, 1000, 450);
+        stage.setTitle("Back To Main Screen");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void onRemoveAssociatedPartBtn(ActionEvent actionEvent) {
     }
 }
