d13ec436faa3bde999a41bc4828b0d4d5e3cdd30
leonredman <leonproart@yahoo.com>
2023-03-04 23:06:13 -0700

    add part feature

--- src/controller/AddPartViewController.java
+++ src/controller/AddPartViewController.java
@@ -1,42 +1,48 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.InHouse;
 import model.Inventory;
-import model.Part;
 import wgu.inventoryApp.MainApplication;
 
 import java.io.IOException;
 
 public class AddPartViewController {
-    public TextArea textArea;
-    public Button secondButton;
-    public RadioButton addPartInHouseYes;
-    public RadioButton addPartOutsourcedYes;
-    public Label addChangeMe;
-    public Button savePartBtn;
-    public TextField partIdTxt;
-    public TextField partNameTxt;
-    public TextField partStockTxt;
-    public TextField partPriceTxt;
-    public TextField partInventoryMaxTxt;
-    public TextField partInventoryMinTxt;
-    public TextField partToggleIdTxt;
-
+    @FXML
+    private RadioButton addPartInHouseYes;
+    @FXML
+    private RadioButton addPartOutsourcedYes;
+    @FXML
+    private Label addChangeMe;
+    @FXML
+    private TextField partIdTxt;
+    @FXML
+    private TextField partNameTxt;
+    @FXML
+    private TextField partStockTxt;
+    @FXML
+    private TextField partInventoryMaxTxt;
+    @FXML
+    private TextField partInventoryMinTxt;
+    @FXML
+    private TextField partToggleIdTxt;
 
 
     public void toMain(ActionEvent actionEvent) throws IOException {
-//        Parent root = FXMLLoader.load(HelloApplication.class.getResource("addPart.fxml "));
-//
+
        Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
        Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-//     Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
 
-        Scene scene = new Scene(root, 1000, 450);
+        Scene scene = new Scene(root, 1200, 450);
         stage.setTitle("Back To Main Screen");
         stage.setScene(scene);
         stage.show();
@@ -50,12 +56,10 @@ public class AddPartViewController {
         addChangeMe.setText("Company Name");
     }
 
-
-
     public void onActionSavePart(ActionEvent actionEvent) throws IOException {
-        int id = Integer.parseInt(partIdTxt.getText());   // using wrapper class to convert input field string to int
+        int id = Inventory.createId();
         String name = partNameTxt.getText();
-        double price = Double.parseDouble(partStockTxt.getText());
+        double price = Double.parseDouble(partStockTxt.getText());  // using wrapper class to convert input field string to int
         int stock = Integer.parseInt(partStockTxt.getText());
         int min = Integer.parseInt(partInventoryMinTxt.getText());;
         int max= Integer.parseInt(partInventoryMaxTxt.getText());;
@@ -66,25 +70,19 @@ public class AddPartViewController {
             isInHouse = true;
         else
             isInHouse = false;
-// ERROR HERE >>>>>
-     Inventory.addPart(new Part(id, name, price, stock, min, max));// we get an error when Part is set to Abstract must refactor here?
-//        Inventory addPart = new Part(id, name, price, stock, min, max);// we get an error when Part is set to Abstract must refactor here?
-//        Part.addPart(addPart);
+
+        // had error here, was trying to instantiate an abstract class Part instead of inHouse which extends part
 
 
+        InHouse addPart = new InHouse(id, name, price, stock, min, max);
+        Inventory.addPart(addPart);
 
-
-
-
-
-
-
+        // alternate method to use Node instead of Button
+        // Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
 
         Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
         Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-//     Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-
-        Scene scene = new Scene(root, 1000, 450);
+        Scene scene = new Scene(root, 1200, 450);
         stage.setTitle("Back To Main Screen");
         stage.setScene(scene);
         stage.show();
