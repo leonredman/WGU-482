f78741539a8ab536aa5396cfe6ce258379b97459
leonredman <leonproart@yahoo.com>
2023-03-11 13:40:21 -0700

    send data to modify form

--- src/controller/ModifyProductViewController.java
+++ src/controller/ModifyProductViewController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -8,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Product;
 import wgu.inventoryApp.MainApplication;
 
 import java.io.IOException;
@@ -15,12 +17,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ModifyProductViewController implements Initializable {
-    public TextField prodModIdLbl;
+    @FXML
+    private TextField prodModIdLbl;
+    @FXML
+    private TextField productModNameLbl;
+    @FXML
+    private TextField productModInventoryLbl;
+    @FXML
+    private TextField productModPriceLbl;
+    @FXML
+    private TextField productModMaxLbl;
+    @FXML
+    private TextField prodModMinLbl;
 
     public void toMainFromModifyProduct(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
-       // Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-    Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        // Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
         Scene scene = new Scene(root, 1200, 450);
         stage.setTitle("Back To Main Screen");
@@ -28,8 +41,47 @@ public class ModifyProductViewController implements Initializable {
         stage.show();
     }
 
+        public void sendProduct(Product product) {
+
+            prodModIdLbl.setText(String.valueOf(product.getId()));
+            productModNameLbl.setText(product.getName());
+            productModInventoryLbl.setText(String.valueOf(product.getStock()));
+            productModPriceLbl.setText(String.valueOf(product.getPrice()));
+            productModMaxLbl.setText(String.valueOf(product.getMax()));
+            prodModMinLbl.setText(String.valueOf(product.getMin()));
+        }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+// todo
+    }
+}
+
+
+
+/*
+Beginning file set up
+    public void toMainFromModifyProduct(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
+        // Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root, 1200, 450);
+        stage.setTitle("Back To Main Screen");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
+
+
+ */
