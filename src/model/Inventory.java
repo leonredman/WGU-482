e4d4976323aade4e488943af04e07da84e029df1
leonredman <leonproart@yahoo.com>
2023-03-13 23:17:18 -0700

    updated methods

--- src/model/Inventory.java
+++ src/model/Inventory.java
@@ -9,7 +9,7 @@ public class Inventory {
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
     private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
 
-    private static ObservableList<Part> filteredParts = FXCollections.observableArrayList();   // filtered parts list
+   // private static ObservableList<Part> filteredParts = FXCollections.observableArrayList();   // filtered parts list
 
 
     public static int nextId = 0;
@@ -51,20 +51,58 @@ public class Inventory {
     }
 
     public static Part lookupPart(int partId) {
-        return null;                                        // need code to return
+        // todo
+        return null;
     }
 
+    public static Product lookupProduct (int productId){
+        // todo
+        return null;
+    }
+
+    public static ObservableList<Part> lookupPart(String partName){
+        // todo
+        //return PartName;
+
+        return null;
+    }
+
+    public static ObservableList<Product> lookupProduct(String productName){
+        // todo
+        //return ProductName;
+
+        return null;
+    }
+
+    public static void updatePart(int index, Part selectedPart) {
+        allParts.set(index, selectedPart);
+    }
+
+
+    public static void updateProduct(int index, Product newProduct) {
+        return ;
+    }
+
+    public static boolean deletePart(Part selectedPart){
+        return true;
+    }
+
+    public static boolean deleteProduct(Product selectedProduct){
+        return true;
+    }
 
     public static ObservableList<Part> getAllParts() {
         return allParts;
     }
 
-    public static ObservableList<Part> getAllFilteredParts() {
-        return filteredParts;
-    }
-
     public static ObservableList<Product> getAllProducts() {
         return allProducts;
     }
+
+//    public static ObservableList<Part> getAllFilteredParts() {
+//        return filteredParts;
+//    }
+
+
 }
 
