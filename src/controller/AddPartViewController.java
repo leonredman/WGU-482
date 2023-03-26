e1d8b9bce01047e8cb66a6dc4ab7ef3d4cd05b48
leonredman <leonproart@yahoo.com>
2023-03-25 23:59:07 -0700

    update features

--- src/controller/AddPartViewController.java
+++ src/controller/AddPartViewController.java
@@ -15,6 +15,7 @@ import wgu.inventoryApp.MainApplication;
 import java.io.IOException;
 
 public class AddPartViewController {
+    public TextField partPriceTxt;
     @FXML
     private RadioButton addPartInHouseYes;
     @FXML
@@ -54,65 +55,53 @@ public class AddPartViewController {
     }
 
     public void onActionSavePart(ActionEvent actionEvent) throws IOException {
+        try {
+            int machineId = 0;
 
-        int machineId = 0;
+            int id = Inventory.createId();
+            String name = partNameTxt.getText();
+            int stock = Integer.parseInt(partStockTxt.getText());  // using wrapper class to convert input field string to int
+            double price = Double.parseDouble(partPriceTxt.getText());
+            int max = Integer.parseInt(partInventoryMaxTxt.getText());
+            int min = Integer.parseInt(partInventoryMinTxt.getText());
 
-        int id = Inventory.createId();
-        String name = partNameTxt.getText();
-        double price = Double.parseDouble(partStockTxt.getText());  // using wrapper class to convert input field string to int
-        int stock = Integer.parseInt(partStockTxt.getText());
-        int max= Integer.parseInt(partInventoryMaxTxt.getText());
-        int min = Integer.parseInt(partInventoryMinTxt.getText());
+            if (max < min) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Maximum must be greater than minimum.");
+                alert.showAndWait();
+                return;
+            }
+            //Inventory should be between the min and max values.
+            else if (stock < min || max < stock) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Inventory must be within min and max.");
+                alert.showAndWait();
+                return;
+            }
 
-        if (max < min) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Maximum must be greater than minimum.");
+            if (addPartInHouseYes.isSelected()) {
+                machineId = Integer.parseInt(partToggleIdTxt.getText());
+                InHouse addPart = new InHouse(id, name, price, stock, max, min, machineId);
+                Inventory.addPart(addPart);
+            } else {
+                String companyName = partToggleIdTxt.getText();
+                Outsourced addPart = new Outsourced(id, name, price, stock, max, min, companyName);
+                System.out.println(companyName);
+                Inventory.addPart(addPart);
+
+            }
+            // had error here, was trying to instantiate an abstract class Part instead of inHouse which extends part
+            Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
+            Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1200, 450);
+            stage.setTitle("Back To Main Screen");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("There is a Field Input Error");
+            alert.setContentText("Please complete and check all your input values and try again");
             alert.showAndWait();
-            return;
         }
-        //Inventory should be between the min and max values.
-        else if (stock < min || max < stock) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Inventory must be within min and max.");
-            alert.showAndWait();
-            return;
-        }
-
-        if(addPartInHouseYes.isSelected()) {
-            machineId = Integer.parseInt(partToggleIdTxt.getText());
-            System.out.println(machineId);   // for debugging
-            InHouse addPart = new InHouse(id, name, price, stock, max, min, machineId);
-            System.out.println(machineId);
-            Inventory.addPart(addPart);
-        }
-
-        else {
-            String companyName = partToggleIdTxt.getText();
-            Outsourced addPart = new Outsourced(id,name, price, stock, max, min, companyName );
-            System.out.println(companyName);
-            Inventory.addPart(addPart);
-
-        }
-
-
-
-
-           // isInHouse = false;
-
-        // had error here, was trying to instantiate an abstract class Part instead of inHouse which extends part
-
-
-        //InHouse addPart = new InHouse(id, name, price, stock, min, max, int machineId);
-//        Inventory.addPart(addPart);
-
-        // alternate method to use Node instead of Button
-        // Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-
-        Parent root = FXMLLoader.load(MainApplication.class.getResource("/view/mainScreen.fxml"));
-        Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 1200, 450);
-        stage.setTitle("Back To Main Screen");
-        stage.setScene(scene);
-        stage.show();
-
     }
 
 }
